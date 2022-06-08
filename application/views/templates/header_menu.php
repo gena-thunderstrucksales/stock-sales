dfe86df5573d193bec71e537c71b1288d0162f52
tsmproduction <moses@thunderstrucksales.com>
2022-06-08 08:50:57 -0500

    header nav and pagination

--- application/views/templates/header_menu.php
+++ application/views/templates/header_menu.php
@@ -13,16 +13,16 @@
 
       <div class="col-xs-6 col-sm-4 col-lg-3 login-menu-top ">
         <div class="row welcome-user">
-          <div class=" col-xs-4 col-sm-3 col-lg-3 min-width-title">
+          <div class="account-links">
             <a class="background-desktop">Welcome</a>
           </div>
-          <div class=" col-xs-4 col-sm-3 col-lg-3 min-width-title">
+          <div class="account-links">
             <a class="user-top-menu" href="<?php echo base_url('users/setting/') ?>"><?php echo $this->session->userdata('username'); ?></a><br>
           </div>
-          <div class="col-xs-1 col-sm-1 col-lg-1 min-width-title">
+          <div class="account-links">
             <div class="userCircle"><?php echo ucfirst(substr($this->session->userdata('username'), 0, 1)); ?></div>
           </div>
-          <div class="col-xs-6 col-sm-6 col-lg-4 min-width-title-sign-out ">
+          <div class="account-links">
               <!-- user permission info -->
               <a class="user-top-menu" href="<?php echo base_url('auth/logout') ?>"><span>Sign Out</span></a>
             </div>
